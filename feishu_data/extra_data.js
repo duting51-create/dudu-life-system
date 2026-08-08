608fb2102576b043c9992ed7b48a7499f33b1480
duting51-create <duting51-create@github.com>
2026-08-08 14:42:10 +0800

    data: auto sync 2026-08-08 14:42

--- feishu_data/extra_data.js
+++ feishu_data/extra_data.js
@@ -172,8 +172,8 @@ window.IMPORTANT_DATES_DATA = {
   ]
 };
 window.DAILY_QUOTE = {
-  "content": "人是悬挂在自己编织的意义之网上的动物。",
-  "source": "文化的阐释"
+  "content": "雁字回时，月满西楼，花自飘零水自流。一种相思，两处闲愁。此情无计可消除，才下眉头，却上心头。",
+  "source": "一剪梅·红藕香残玉簟秋"
 };
 window.updated = {
   "name": "嗨咻",
