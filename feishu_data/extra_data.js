519026fbc0633b6c28f579b4d1c73e3d3cc05ca3
duting51-create <duting51-create@github.com>
2026-08-22 12:10:48 +0800

    data: auto sync 2026-08-22 12:10

--- feishu_data/extra_data.js
+++ feishu_data/extra_data.js
@@ -1,6 +1,6 @@
 // 新增模块静态数据（由 feishu_sync.py 生成 / 手动维护）
 window.PODCASTS_DATA = {
-  "updated": "2026-08-21",
+  "updated": "2026-08-22",
   "list": [
     {
       "name": "嗨咻",
@@ -61,7 +61,7 @@ window.INVEST_DATA = {
   ]
 };
 window.IMPORTANT_DATES_DATA = {
-  "updated": "2026-08-21",
+  "updated": "2026-08-22",
   "list": [
     {
       "name": "爽",
@@ -172,8 +172,8 @@ window.IMPORTANT_DATES_DATA = {
   ]
 };
 window.DAILY_QUOTE = {
-  "content": "近乡情更怯，不敢问来人。",
-  "source": "渡汉江"
+  "content": "Everything ends. But everything begins again too.",
+  "source": "奇异博士"
 };
 window.updated = {
   "name": "嗨咻",
