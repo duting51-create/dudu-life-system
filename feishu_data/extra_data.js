56a8dd0f004f6891c5f66161d6979feb1a07bf54
duting51-create <duting51-create@github.com>
2026-08-27 15:03:19 +0800

    data: auto sync 2026-08-27 15:03

--- feishu_data/extra_data.js
+++ feishu_data/extra_data.js
@@ -4,7 +4,7 @@ window.PODCASTS_DATA = {
   "list": [
     {
       "name": "嗨咻",
-      "latest": "103.和海狸博士聊天｜关上门后，我终于敢说“我想要”"
+      "latest": "最新单集加载中…"
     },
     {
       "name": "头回当妈",
@@ -12,7 +12,7 @@ window.PODCASTS_DATA = {
     },
     {
       "name": "畅所育言",
-      "latest": "113入园大作战：准备好书包，也准备好第一次告别"
+      "latest": "最新单集加载中…"
     },
     {
       "name": "当个事儿",
@@ -20,7 +20,7 @@ window.PODCASTS_DATA = {
     },
     {
       "name": "Alison Yu心理",
-      "latest": "ep92 | 我们的每一个选择，都在塑造未来的自己"
+      "latest": "最新单集加载中…"
     }
   ]
 };
@@ -171,10 +171,7 @@ window.IMPORTANT_DATES_DATA = {
     }
   ]
 };
-window.DAILY_QUOTE = {
-  "content": "生命中所有的绚烂，原来终究，都要用寂寞来偿还。",
-  "source": "百年孤独"
-};
+window.DAILY_QUOTE = null;
 window.updated = {
   "name": "嗨咻",
   "latest": "100. 为什么越想表现自己，越容易在社交中露怯？"
