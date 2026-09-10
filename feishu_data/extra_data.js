b59debe42c6aac907a7eb15f7d23f05798b91c7c
duting51-create <duting51-create@github.com>
2026-09-10 15:39:40 +0800

    data: auto sync 2026-09-10 15:39

--- feishu_data/extra_data.js
+++ feishu_data/extra_data.js
@@ -4,7 +4,7 @@ window.PODCASTS_DATA = {
   "list": [
     {
       "name": "嗨咻",
-      "latest": "105. 人到中年，又累又不快乐"
+      "latest": "最新单集加载中…"
     },
     {
       "name": "头回当妈",
@@ -171,7 +171,10 @@ window.IMPORTANT_DATES_DATA = {
     }
   ]
 };
-window.DAILY_QUOTE = null;
+window.DAILY_QUOTE = {
+  "content": "应是天仙狂醉，乱把白云揉碎。",
+  "source": "清平乐·画堂晨起"
+};
 window.updated = {
   "name": "嗨咻",
   "latest": "100. 为什么越想表现自己，越容易在社交中露怯？"
