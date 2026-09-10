3a36339a635e0b788a9e2984e31a69bdb2835088
duting51-create <duting51-create@github.com>
2026-09-10 21:01:21 +0800

    data: auto sync 2026-09-10 21:01

--- feishu_data/extra_data.js
+++ feishu_data/extra_data.js
@@ -4,7 +4,7 @@ window.PODCASTS_DATA = {
   "list": [
     {
       "name": "嗨咻",
-      "latest": "最新单集加载中…"
+      "latest": "105. 人到中年，又累又不快乐"
     },
     {
       "name": "头回当妈",
@@ -12,7 +12,7 @@ window.PODCASTS_DATA = {
     },
     {
       "name": "畅所育言",
-      "latest": "最新单集加载中…"
+      "latest": "114《为父则刚》：不是“我帮你带娃”，而是我们一起成为父母｜新晋待产包必备好物"
     },
     {
       "name": "当个事儿",
@@ -20,7 +20,7 @@ window.PODCASTS_DATA = {
     },
     {
       "name": "Alison Yu心理",
-      "latest": "最新单集加载中…"
+      "latest": "社会化：我们拥有比自己想象中更多的选择"
     }
   ]
 };
@@ -172,8 +172,8 @@ window.IMPORTANT_DATES_DATA = {
   ]
 };
 window.DAILY_QUOTE = {
-  "content": "休对故人思故国，且将新火试新茶。诗酒趁年华。",
-  "source": "望江南·超然台作"
+  "content": "Why so serious?",
+  "source": "小丑"
 };
 window.updated = {
   "name": "嗨咻",
