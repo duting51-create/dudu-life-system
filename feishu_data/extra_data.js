9f547d405315c68ead3e80b2e7fc3fe99d8d8d85
duting51-create <duting51-create@github.com>
2026-09-10 17:49:00 +0800

    data: auto sync 2026-09-10 17:49

--- feishu_data/extra_data.js
+++ feishu_data/extra_data.js
@@ -20,7 +20,7 @@ window.PODCASTS_DATA = {
     },
     {
       "name": "Alison Yu心理",
-      "latest": "最新单集加载中…"
+      "latest": "社会化：我们拥有比自己想象中更多的选择"
     }
   ]
 };
@@ -172,8 +172,8 @@ window.IMPORTANT_DATES_DATA = {
   ]
 };
 window.DAILY_QUOTE = {
-  "content": "风啊，你还要吹到何时......",
-  "source": "卡尔·冯·沃尔里奇"
+  "content": "我眺望远方的山峰，却错过转弯的路口，蓦然回首，才发现你在等我，没离开过。",
+  "source": "没离开过"
 };
 window.updated = {
   "name": "嗨咻",
